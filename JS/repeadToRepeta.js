89eefe56298f45a834d143dc9cd2104979288a9c
Vadim <vyakushonok@gmail.com>
2021-04-05 09:54:00 +0300

    тренюсь

--- JS/repeadToRepeta.js
+++ JS/repeadToRepeta.js
@@ -1,4 +1,43 @@
-// повтор за Репетой второго модуля вспомнить ВСЕ!!!!
+// повтор за Репетой 2-го модуля вспомнить ВСЕ!!!!
+/* const friends = ['Mango', 'Kivi', 'Poly', 'Ajax'];
+
+console.log(friends);
+console.table(friends);
+
+console.log(friends.length);
+
+const lastIndex = friends.length - 1;
+console.log(lastIndex);
+
+console.log(friends[2]);
+
+friends[1] = 'jhbdhb';
+console.log(friends);
+
+// Перебор массива если нужно знать индекс
+
+for (let i = 0; i <= friends.length - 1; i +=1) {
+    console.log(i, friends[i]);
+/*     console.log(i); */
+/* };
+
+for (const friend of friends) {
+    console.log(friend);
+}; */ 
+
+// Посчитать общую сумму покупок в корзине
+
+const carts = [54, 28, 105, 70, 92, 17, 120];
+let total = 0;
+
+for (const cart of carts) {
+total += cart;
+}
+
+console.log(total);
+
+
+
 
 
 

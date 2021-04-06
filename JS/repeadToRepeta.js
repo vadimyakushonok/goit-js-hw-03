bb8b495e827b6c88db5ceba7d6f595397002770c
Vadim <vyakushonok@gmail.com>
2021-04-06 10:45:03 +0300

    снова тренюсь по репете

--- JS/repeadToRepeta.js
+++ JS/repeadToRepeta.js
@@ -27,14 +27,77 @@ for (const friend of friends) {
 
 // Посчитать общую сумму покупок в корзине
 
-const carts = [54, 28, 105, 70, 92, 17, 120];
-let total = 0;
+// const carts = [54, 28, 105, 70, 92, 17, 120];
+// let total = 0;
 
-for (const cart of carts) {
-total += cart;
+// for (const cart of carts) {
+// total += cart;
+// }
+
+// console.log(total);
+
+// // Изпользуя счетчик изменить элекменты массива
+
+// for (let cart = 0; cart < carts.length; cart += 1) {
+//     carts[cart] = Math.round(carts[cart] * 1.1);
+// }
+
+// console.log(carts);
+
+// // Напиши скрипт который подсчитывает значения всех четных чисел в массиве
+
+// const numbers = [1, 2, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+// let totalEven = 0;
+// let totalUneven = 0;
+
+
+// for (let number = 0; number < numbers.length; number += 1) {
+//     if (numbers[number] % 2 === 0) {
+//         totalEven += numbers[number];
+//     }
+//     else {
+//         totalUneven += numbers[number]
+//     }
+
+// }
+// console.log(`сумма Четное: ${totalEven}, сумма Нечетное: ${totalUneven}`);
+
+
+// Напиши скрипт поиска логина 
+// Если логина нет, вывести сообщение "Пользователь [логин] не найлен"
+// Если нашли логин, вывести сообщение "Пользователь [логин] найден"
+
+// const logins = ['mango', 'kiwi', 'poly', 'ajax'];
+// const loginToFind = 'poly';
+// let message = '';
+
+// for (let i = 0; i < logins.length; i += 1) {
+//     const login = logins[i];
+//     if (login === loginToFind) {
+//         message = `Пользователь ${loginToFind} найден`;
+//         break;
+//     }
+//     message = `Пользователь ${loginToFind} НЕ найден`;
+//  }
+
+// const message = logins.includes(loginToFind) 
+//     ? `Пользователь ${loginToFind} НАЙДЕН`
+//     : `Пользователь ${loginToFind} НЕ Найден`;
+
+// console.log(message);
+
+const numbers = [51, 18, 13, 24, 7, 85, 19];
+let smallestNumber = numbers[0];
+
+for (const number of numbers) {
+    if (number < smallestNumber) {
+        smallestNumber = number;
+    }
 }
 
-console.log(total);
+console.log(smallestNumber);
+
+
 
 
 

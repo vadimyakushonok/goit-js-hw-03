872cd8441ad97969ce60f8147e48c792f8ef61f0
Vadim <vyakushonok@gmail.com>
2021-08-05 22:07:43 +0300

    снова комит

--- JS/repadToVenya_1_2.js
+++ JS/repadToVenya_1_2.js
@@ -1,3 +1,25 @@
-Пример 1 цикл for
+/* Пример 1 цикл for
 Напишите цикл for который выводит в консоль браузера числа по 
-возрастанию от min до max
+возрастанию от min до max */
+
+// let count = 1;
+// let a = count++;
+
+// console.log(a);
+
+// a = ++count;
+// console.log(a);
+
+const s = 'jjj';
+let result = '';
+
+for (let i = 0; i < s.length; i++) {
+    if (i === 0) {
+        result += s[i].toUpperCase();
+      continue;
+    }
+    result += s[i];
+
+}
+
+console.log(result);

0ea70dabe55fa2457cb7c13427e7807ab0858d60
Vadim <vyakushonok@gmail.com>
2021-08-10 22:23:02 +0300

    Update repadToVenya_1_2.js

--- JS/repadToVenya_1_2.js
+++ JS/repadToVenya_1_2.js
@@ -10,7 +10,7 @@
 // a = ++count;
 // console.log(a);
 
-const s = 'jjj';
+/* const s = 'jjj';
 let result = '';
 
 for (let i = 0; i < s.length; i++) {
@@ -22,4 +22,54 @@ for (let i = 0; i < s.length; i++) {
 
 }
 
-console.log(result);
+console.log(result); */
+
+// Пример №1 - Цикл for
+// Напишите цикл for  который выводит в консоль браузера 
+// числа по возрастанию от min  до max
+
+/* const max = 100;
+const min = 20;
+
+for (let i = min; i <= max; i++) {
+  console.log(i);
+} */
+
+// Пример №2 - цикл for
+// Напишите цикл for который выводит в консоль браузера числа по убыванию
+// от max до min
+
+/* const max = 100;
+const min = 20;
+
+for (let i = max; i >= min; i--) {
+  console.log(i);
+}; */
+
+// Пример 3 - Цикл for
+// При помощи цикла for выведите четные числа от 2 до 10.
+// Решение 2: c continue
+// Решение 3: меньше кода и итерации
+
+const max = 10;
+const min = 2;
+
+// Решение 1: больше кода и итерации 
+for (let i = min; i <= max; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// Решение 2: c continue
+for (let i = min; i <= max; i++) {
+  if (i % 2 !== 0) {
+    continue
+  };
+  console.log(i);
+}
+
+// Решение 3: меньше кода и итерации
+for (let i = min; i <= max; i +=  2) {
+
+}
